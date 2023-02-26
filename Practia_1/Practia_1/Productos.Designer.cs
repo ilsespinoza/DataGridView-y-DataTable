f93ecc3f69ba323a87841f9b8927dfa60c17cca1
ilsespinoza <ilseesflo27@gmail.com>
2023-02-26 14:57:38 -0700

    Se agrega el proyecto

---
+++ Practia_1/Practia_1/Productos.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Practia_1
+{
+    partial class PRODUCTOS
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.Agregar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.Actualizar = new System.Windows.Forms.Button();
+            this.txtIDProducto = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkActivo = new System.Windows.Forms.CheckBox();
+            this.IDProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Activo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AllowUserToAddRows = false;
+            this.dgvProductos.AllowUserToDeleteRows = false;
+            this.dgvProductos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IDProducto,
+            this.Nombre,
+            this.Cantidad,
+            this.Precio,
+            this.Activo});
+            this.dgvProductos.Location = new System.Drawing.Point(12, 12);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.ReadOnly = true;
+            this.dgvProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProductos.Size = new System.Drawing.Size(776, 251);
+            this.dgvProductos.TabIndex = 0;
+            this.dgvProductos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProductos_CellContentClick);
+            this.dgvProductos.SelectionChanged += new System.EventHandler(this.dgvProductos_SelectionChanged);
+            this.dgvProductos.Layout += new System.Windows.Forms.LayoutEventHandler(this.dgvProductos_Layout);
+            // 
+            // Agregar
+            // 
+            this.Agregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Agregar.ForeColor = System.Drawing.Color.Fuchsia;
+            this.Agregar.Location = new System.Drawing.Point(211, 384);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(99, 44);
+            this.Agregar.TabIndex = 1;
+            this.Agregar.Text = "AGREGAR";
+            this.Agregar.UseVisualStyleBackColor = false;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Eliminar.ForeColor = System.Drawing.Color.Purple;
+            this.Eliminar.Location = new System.Drawing.Point(498, 383);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(99, 44);
+            this.Eliminar.TabIndex = 2;
+            this.Eliminar.Text = "ELIMINAR";
+            this.Eliminar.UseVisualStyleBackColor = false;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // Actualizar
+            // 
+            this.Actualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Actualizar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Actualizar.Location = new System.Drawing.Point(352, 383);
+            this.Actualizar.Name = "Actualizar";
+            this.Actualizar.Size = new System.Drawing.Size(102, 45);
+            this.Actualizar.TabIndex = 3;
+            this.Actualizar.Text = "ACTUALIZAR";
+            this.Actualizar.UseVisualStyleBackColor = false;
+            this.Actualizar.Click += new System.EventHandler(this.Actualizar_Click);
+            // 
+            // txtIDProducto
+            // 
+            this.txtIDProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtIDProducto.Location = new System.Drawing.Point(22, 311);
+            this.txtIDProducto.Name = "txtIDProducto";
+            this.txtIDProducto.ReadOnly = true;
+            this.txtIDProducto.Size = new System.Drawing.Size(141, 20);
+            this.txtIDProducto.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtNombre.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtNombre.Location = new System.Drawing.Point(211, 311);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(170, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtCantidad.Location = new System.Drawing.Point(407, 311);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(145, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txtPrecio.ForeColor = System.Drawing.Color.Black;
+            this.txtPrecio.Location = new System.Drawing.Point(601, 311);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(138, 20);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label.Location = new System.Drawing.Point(77, 280);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(79, 13);
+            this.label.TabIndex = 8;
+            this.label.Text = "IDPRODUCTO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(267, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "NOMBRE";
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.txt.Location = new System.Drawing.Point(453, 280);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(62, 13);
+            this.txt.TabIndex = 10;
+            this.txt.Text = "CANTIDAD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(617, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "PRECIO";
+            // 
+            // chkActivo
+            // 
+            this.chkActivo.AutoSize = true;
+            this.chkActivo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.chkActivo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.chkActivo.Location = new System.Drawing.Point(363, 351);
+            this.chkActivo.Name = "chkActivo";
+            this.chkActivo.Size = new System.Drawing.Size(91, 17);
+            this.chkActivo.TabIndex = 12;
+            this.chkActivo.Text = "¿Esta activo?";
+            this.chkActivo.UseVisualStyleBackColor = false;
+            // 
+            // IDProducto
+            // 
+            this.IDProducto.HeaderText = "IDProducto";
+            this.IDProducto.Name = "IDProducto";
+            this.IDProducto.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.ReadOnly = true;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            // 
+            // Activo
+            // 
+            this.Activo.HeaderText = "Activo";
+            this.Activo.Name = "Activo";
+            this.Activo.ReadOnly = true;
+            this.Activo.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Activo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // PRODUCTOS
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkActivo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtIDProducto);
+            this.Controls.Add(this.Actualizar);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.Agregar);
+            this.Controls.Add(this.dgvProductos);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "PRODUCTOS";
+            this.Text = "PRODUCTOS";
+            this.Load += new System.EventHandler(this.PRODUCTOS_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Button Agregar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button Actualizar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Activo;
+        private System.Windows.Forms.TextBox txtIDProducto;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkActivo;
+    }
+}
+
---
+++ Practia_1/Practia_1/Productos.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practia_1
+{
+    public partial class PRODUCTOS : Form
+    {
+
+        private int contador = 1;
+        public PRODUCTOS()
+        {
+            InitializeComponent();
+
+        }
+
+        private void dgvProductos_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void PRODUCTOS_Load(object sender, EventArgs e)
+        {
+
+
+
+            dgvProductos.Rows.Add(new Object[] { contador++, "Lapiz", 10, 12.5, true });
+            dgvProductos.Rows.Add(new Object[] { contador++, "Pluma", 15, 14.80, true });
+
+
+
+        }
+
+
+
+
+
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvProductos.Rows.Add(new object[] { contador++,
+                  txtNombre.Text,txtCantidad.Text,txtPrecio.Text,chkActivo.Checked});
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Excepción: " + ex.ToString());
+            }
+        }
+    
+
+        private void dgvProductos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Actualizar_Click(object sender, EventArgs e)
+        {
+            if (dgvProductos.SelectedRows.Count > 0)
+            {
+                DataGridViewRow renglon = dgvProductos.SelectedRows[0];
+                renglon.Cells["Nombre"].Value = txtNombre.Text;
+                renglon.Cells["Cantidad"].Value = txtCantidad.Text;
+                renglon.Cells["Precio"].Value = double.Parse(txtPrecio.Text);
+                renglon.Cells["Activo"].Value = chkActivo.Checked;
+            }
+        }
+
+        private void Eliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvProductos.Rows.Remove(dgvProductos.SelectedRows[0]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Excepción: " + ex.ToString());
+            }
+        }
+    
+
+        private void dgvProductos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvProductos.SelectedRows.Count > 0)
+            {
+
+
+                DataGridViewRow renglon = dgvProductos.SelectedRows[0];
+                txtIDProducto.Text = renglon.Cells["IDProducto"].Value.ToString();
+                txtNombre.Text = renglon.Cells["Nombre"].Value.ToString();
+                txtCantidad.Text = renglon.Cells["Cantidad"].Value.ToString();
+                txtPrecio.Text = renglon.Cells["Precio"].Value.ToString();
+                chkActivo.Checked = bool.Parse(renglon.Cells["Activo"].Value.ToString());
+
+            }
+        }
+    }
+}
+
+
